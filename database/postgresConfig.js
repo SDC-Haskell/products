b12dbdd7c59b1709de8e8f48254be29dee980801
Darian Chan <darian.s.chan@gmail.com>
2021-03-18 12:01:47 -0700

    handled products api route

--- database/postgresConfig.js
+++ database/postgresConfig.js
@@ -10,22 +10,40 @@ const config = {
 
 const client = new Client(config);
 
-const connection = client.connect();
+client.connect().then(()=> console.log("connected"))
 
-const getFirstFive = function (req, res) {
+const getProducts = function (req, res) {
+  // req.query is an object with {count:"1", page:"2"}
+  // starting page for product is 17067. set 1 = 17067?
 
   let count = 5;
   let page = 1;
+  // if page = 2, start at item 6
+  // if page = 3, start at item 11
 
-  connection.query(`select * from products limit 5`, (error,result) => {
-    res.send(result)
+  if (req.query.count) {
+    count = Number(req.query.count)
+  }
+  if (req.query.page) {
+    page = Number(req.query.page)
+  }
+
+  // client.query(`select * from products limit ${count}`, (error, results) => {
+  //   res.send(results)
+  // })
+
+  client.query(`select * from products where product_id between ${page} and ${page + count - 1}`, (error, results) => {
+    if (error) {
+      res.send(error)
+    } else {
+      res.send(results.rows)
+    }
   })
 }
 
 
 module.exports = {
-  connection,
-  getFirstFive
+  getProducts
 }
 
 
--- server/index.js
+++ server/index.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const connection = require('../database/postgresConfig.js')
+const { Client } = require('pg');
 
 const app = express()
 const port = 3000
 
-app.get('/products', connection.getFirstFive)
+app.get('/products', connection.getProducts)
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
